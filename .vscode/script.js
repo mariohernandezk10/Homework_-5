6f1a12147edad70822e7cae3d5126797979a2a96
Mario Hernandez <mariohernandezk10@gmail.com>
2020-11-11 23:51:30 -0600

    added get from local storage

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,10 +1,17 @@
-//get from local storage using id
-$("#9").val(localStorage.getItem("9"));
-$("#10").val(localStorage.getItem("10"));
-$("#11").val(localStorage.getItem("11"));
-$("#12").val(localStorage.getItem("12"));
-$("#13").val(localStorage.getItem("13"));
-$("#14").val(localStorage.getItem("14"));
-$("#15").val(localStorage.getItem("15"));
-$("#16").val(localStorage.getItem("16"));
-$("#17").val(localStorage.getItem("17"));
+//get from local storage
+// $("#9").val(localStorage.getItem("9"));
+// $("#10").val(localStorage.getItem("10"));
+// $("#11").val(localStorage.getItem("11"));
+// $("#12").val(localStorage.getItem("12"));
+// $("#13").val(localStorage.getItem("13"));
+// $("#14").val(localStorage.getItem("14"));
+// $("#15").val(localStorage.getItem("15"));
+// $("#16").val(localStorage.getItem("16"));
+// $("#17").val(localStorage.getItem("17"));
+
+// set to local storage
+// local storage has a key and value
+// the value = user inputs
+// the key = time
+let value = $(document).body;
+console.log(value);
