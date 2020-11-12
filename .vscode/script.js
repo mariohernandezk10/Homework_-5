98d7b87fb9b0d9dcaf2173549776c53988eac595
Mario Hernandez <mariohernandezk10@gmail.com>
2020-11-11 23:30:01 -0600

    get from localstorage

---
+++ .vscode/script.js
@@ -0,0 +1,10 @@
+//get from local storage using id
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+$("#11").val(localStorage.getItem("11"));
+$("#12").val(localStorage.getItem("12"));
+$("#13").val(localStorage.getItem("13"));
+$("#14").val(localStorage.getItem("14"));
+$("#15").val(localStorage.getItem("15"));
+$("#16").val(localStorage.getItem("16"));
+$("#17").val(localStorage.getItem("17"));
