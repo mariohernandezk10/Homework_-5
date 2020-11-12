7cd4194dfb3ec893d490393d0a9b0c432baf69f5
Mario Hernandez <mariohernandezk10@gmail.com>
2020-11-12 01:45:24 -0600

    deleted unnecessary code

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-//get from local storage
-// recall getItem "gets the key"
 
-// we need to add the set interval time
-// first we need to get the correct format
 setInterval(function() {
     $(".today").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 }, 1)
@@ -31,21 +27,12 @@ $(".saveBtn").click(function () {
     localStorage.setItem(timeKey, textValue);
 });
 
-const div1 = $(document)[0].body.children[1].children[0];
-const div2 = $(document)[0].body.children[1].children[1];
-const div3 = $(document)[0].body.children[1].children[2];
-const div4 = $(document)[0].body.children[1].children[3];
-const div5 = $(document)[0].body.children[1].children[4];
-const div6 = $(document)[0].body.children[1].children[5];
-const div7 = $(document)[0].body.children[1].children[6];
-const div8 = $(document)[0].body.children[1].children[7];
-const div9 = $(document)[0].body.children[1].children[8];
-
 // console.log(div8);
 
 let arrayTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-// let value = arrayTime.forEach(function () {});
+let value = arrayTime.forEach(function () {});
+console.log(value[i])
 
 
 
