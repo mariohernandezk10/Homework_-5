854588dc3ac49b508f59d006fc295a6808c38ba6
Mario Hernandez <mariohernandezk10@gmail.com>
2020-11-12 01:52:25 -0600

    deleted code

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-
 setInterval(function() {
     $(".today").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-}, 1)
-
-
+}, 1);
 
 
 $("#9 .description").val(localStorage.getItem("9"));
@@ -16,10 +13,6 @@ $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
 
-// set to local storage
-// local storage has a key and value
-// the value = user inputs
-// the key = time
 
 $(".saveBtn").click(function () {
 	let textValue = $(this).siblings(".description").val();
@@ -27,24 +20,12 @@ $(".saveBtn").click(function () {
     localStorage.setItem(timeKey, textValue);
 });
 
-// console.log(div8);
-
 let arrayTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 let value = arrayTime.forEach(function () {});
-console.log(value[i])
+// console.log(value[i]);
 
 
 
 
 
-
-const text1 = div1.children[1];
-const text2 = div2.children[1];
-const text3 = div3.children[1];
-const text4 = div4.children[1];
-const text5 = div5.children[1];
-const text6 = div6.children[1];
-const text7 = div7.children[1];
-const text8 = div8.children[1];
-const text9 = div9.children[1];
